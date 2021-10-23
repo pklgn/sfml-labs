ff5a7481bfeb17295f0e863b052bf6a650a8b1a7
pklgn <petechka_petrov_201@mail.ru>
2021-10-23 16:15:07 +0300

    added A+B program

---
+++ lw2/sfml.1/00/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    int a = 0;
+    int b = 0;
+    std::cout << "please, enter two numbers" << std::endl;
+    std::cin >> a >> b;
+    std::cout << "sum of two numbers is " << (a + b) << std::endl;
+}
